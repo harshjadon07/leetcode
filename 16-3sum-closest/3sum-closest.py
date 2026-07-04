6cf994a95cc008df703622e57508b80055c5762a
harshjadon07 <jadounharshkumar06@gmail.com>
2026-07-04 16:05:02 +0530

    Time: 386 ms (50.01%) | Memory: 19.2 MB (80.57%) - LeetSync

---
+++ 16-3sum-closest/3sum-closest.py
@@ -0,0 +1,23 @@
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        n = len(nums)
+        closest = nums[0] + nums[1] + nums[2]
+
+        for i in range(n - 2):
+            left, right = i + 1, n - 1
+
+            while left < right:
+                curr = nums[i] + nums[left] + nums[right]
+
+                if abs(curr - target) < abs(closest - target):
+                    closest = curr
+
+                if curr < target:
+                    left += 1
+                elif curr > target:
+                    right -= 1
+                else:
+                    return target
+
+        return closest

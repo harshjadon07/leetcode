52b49aded9bbd925b53de9cde2ffdd28e0787f3c
harshjadon07 <jadounharshkumar06@gmail.com>
2026-07-17 15:34:51 +0530

    Time: 43 ms (29.59%) | Memory: 19.2 MB (66.71%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.py
@@ -0,0 +1,27 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        if num1 == "0" or num2 == "0":
+            return "0"
+
+        m, n = len(num1), len(num2)
+        res = [0] * (m + n)
+
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                mul = (ord(num1[i]) - ord('0')) * (ord(num2[j]) - ord('0'))
+
+                p1 = i + j
+                p2 = i + j + 1
+
+                total = mul + res[p2]
+
+                res[p2] = total % 10
+                res[p1] += total // 10
+
+        ans = []
+        for digit in res:
+            if not ans and digit == 0:
+                continue
+            ans.append(str(digit))
+
+        return "".join(ans)

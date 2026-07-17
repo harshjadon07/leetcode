ddcbcc2706a65aca8c93525096a651e009ebb710
harshjadon07 <jadounharshkumar06@gmail.com>
2026-07-17 15:37:33 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (33.50%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        path = []
+        used = [False] * len(nums)
+
+        def backtrack():
+            if len(path) == len(nums):
+                ans.append(path[:])
+                return
+
+            for i in range(len(nums)):
+                if used[i]:
+                    continue
+
+                used[i] = True
+                path.append(nums[i])
+
+                backtrack()
+
+                path.pop()
+                used[i] = False
+
+        backtrack()
+        return ans

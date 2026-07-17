51e5048e1a7de1cf5ce523f6264641843dd89679
harshjadon07 <jadounharshkumar06@gmail.com>
2026-07-17 15:30:11 +0530

    Time: 523 ms (57.50%) | Memory: 38.9 MB (100.00%) - LeetSync

---
+++ 3583-sorted-gcd-pair-queries/sorted-gcd-pair-queries.py
@@ -0,0 +1,46 @@
+from typing import List
+from bisect import bisect_right
+
+class Solution:
+    def gcdValues(self, nums: List[int], queries: List[int]) -> List[int]:
+        mx = max(nums)
+
+        # Frequency of each number
+        freq = [0] * (mx + 1)
+        for x in nums:
+            freq[x] += 1
+
+        # cnt[d] = numbers divisible by d
+        cnt = [0] * (mx + 1)
+        for d in range(1, mx + 1):
+            for multiple in range(d, mx + 1, d):
+                cnt[d] += freq[multiple]
+
+        # exact[d] = pairs with gcd exactly d
+        exact = [0] * (mx + 1)
+
+        for d in range(mx, 0, -1):
+            c = cnt[d]
+            pairs = c * (c - 1) // 2
+            multiple = 2 * d
+            while multiple <= mx:
+                pairs -= exact[multiple]
+                multiple += d
+            exact[d] = pairs
+
+        # Prefix counts of sorted gcdPairs
+        prefix = []
+        values = []
+        total = 0
+        for d in range(1, mx + 1):
+            if exact[d]:
+                total += exact[d]
+                prefix.append(total)
+                values.append(d)
+
+        ans = []
+        for q in queries:
+            idx = bisect_right(prefix, q)
+            ans.append(values[idx])
+
+        return ans

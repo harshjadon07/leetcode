0c78cd43f74f1d8943eb635aece30aaa7e5a668d
harshjadon07 <jadounharshkumar06@gmail.com>
2026-07-17 15:35:34 +0530

    Time: 735 ms (32.43%) | Memory: 19.3 MB (88.23%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.py
+++ 44-wildcard-matching/wildcard-matching.py
@@ -1,10 +1,11 @@
 class Solution:
-    def isMatch(self, s, p):
+    def isMatch(self, s: str, p: str) -> bool:
         m, n = len(s), len(p)
 
         prev = [False] * (n + 1)
         prev[0] = True
 
+        # Empty string vs pattern
         for j in range(1, n + 1):
             if p[j - 1] == '*':
                 prev[j] = prev[j - 1]
@@ -17,6 +18,8 @@ class Solution:
                     curr[j] = prev[j - 1]
                 elif p[j - 1] == '*':
                     curr[j] = curr[j - 1] or prev[j]
+                else:
+                    curr[j] = False
 
             prev = curr
 
